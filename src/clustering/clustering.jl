f22d15863687f5c1bbbbd0c1c77d49e4e213ab4a
James Cai <jamescai@genomezoo.net>
2021-01-21 14:12:58 -0600

    Update clustering.jl
    @JuliaRegistrator register()

--- src/clustering/clustering.jl
+++ src/clustering/clustering.jl
@@ -2,10 +2,15 @@ module Clustering
 
 using Clustering
 
-export kmeans
+export kmeans,
+       affprop
 
 function kmeans(X, k)
     Clustering.kmeans(X, k);
+end
+
+function affprop(X, k)
+    Clustering.affprop(X, k);
 end
 
 end
