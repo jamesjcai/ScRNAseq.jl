39c6fbc7ac72e7151930da9e1ff067cba70648f3
James Cai <jamescai@genomezoo.net>
2021-01-22 10:52:02 -0600

    Update normalization.jl

--- src/normalization/normalization.jl
+++ src/normalization/normalization.jl
@@ -4,9 +4,6 @@ export
 norm_libsize,
 norm_deseq
 
-include("../differentialexpression/differentialexpression.jl")
-
-using .DifferentialExpression
 
 function norm_libsize(X)
     lbsz=sum(X,dims=1)
@@ -14,8 +11,14 @@ function norm_libsize(X)
     return X
 end
 
-function norm_deseq(X)
-    X = DifferentialExpression.median_of_ratios_normalization(X);
-    return X
-end
+# Not sure how to include another function in the same package -- pls help!
+#
+# include("../differentialexpression/differentialexpression.jl")
+# using .DifferentialExpression
+
+# function norm_deseq(X)
+#    X = DifferentialExpression.median_of_ratios_normalization(X);
+#    return X
+# end
+
 end
